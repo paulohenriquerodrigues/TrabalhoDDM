f3189b7b2ddd48374b4e4ebe1766fd2956242613
Bruno Rech <brunogrech@hotmail.com>
2019-10-02 11:44:45 -0300

    Adição do menu e listView

--- app/src/main/java/br/udesc/acheaqui/HomeActivity.java
+++ app/src/main/java/br/udesc/acheaqui/HomeActivity.java
@@ -1,31 +1,62 @@
 package br.udesc.acheaqui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button bt_service;
+
+    ListView servicesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        bt_service = (Button) findViewById(R.id.bt_service);
+        servicesList = (ListView) findViewById(R.id.servicesList);
 
-        bt_service.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        List<String> testes = new ArrayList();
+        testes.add("ITEM");
+        testes.add("ITEM");
+        testes.add("ITEM");
+        testes.add("ITEM");
+        testes.add("ITEM");
 
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, testes);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        servicesList.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addservice:
                 Intent i = new Intent(HomeActivity.this, activity_cadastro_servico.class);
                 startActivity(i);
-
-            }
-        });
+                return true;
+            default:
+                return true;
+        }
     }
 }
