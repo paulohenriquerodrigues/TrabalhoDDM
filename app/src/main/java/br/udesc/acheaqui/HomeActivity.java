4ad443bb90a54f73e139051496e5fa2d16de749e
brunorech <brunogrech@hotmail.com>
2019-11-22 17:36:26 -0300

    Layout changes

--- app/src/main/java/br/udesc/acheaqui/HomeActivity.java
+++ app/src/main/java/br/udesc/acheaqui/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
